81802b8d858d6221589ee7c91c475174202b2ac0
Riccardo Eickvonder <riccardo.eickvonder@sgsdd-students.com>
2020-09-28 17:37:57 +0000

    added bool value to check if the combination is possible

--- main.cs
+++ main.cs
@@ -8,8 +8,14 @@ class MainClass {
   }
 
   public static void CN (){
+    var pos = true;
     Write ("Enter the Number of Nuggets: "); 
     int CNN = Convert.ToInt32(ReadLine()); //takes in the input string and converts it to a integer
+    if (CNN < 8){
+      WriteLine ("No Possible Combinations");
+      CNN = 0;
+      pos = false;
+    }
     int count20 = 0;
     int count9 = 0;
     int count6 = 0;
@@ -19,7 +25,7 @@ class MainClass {
           CNN -= 20;
           count20 += 1;
         }
-        else if (CNN >= 13  && CNN != 14 || CNN == 9  ){
+        else if (CNN >= 13  && CNN != 14 && CNN != 16|| CNN == 9  ){
           CNN -= 9;
           count9 += 1;
         }
@@ -34,12 +40,15 @@ class MainClass {
         else if (CNN < 8){
           WriteLine ("No Possible Combinations");
           CNN = 0;
+          pos = false;
         }
     }
+    if (pos == true){
     WriteLine (@"You need:
    {0} boxes of 20
    {1} boxes of 9
    {2} boxes of 6
    {3} boxes of 4", count20 , count9, count6, count4);
+    }
   }
 }
